a304d3d40481fc4364e9cefeecc72afa73f8a42d
Joe Reid <jreid@vnet.net>
2014-06-22 11:58:56 -0500

    initial commit of FinalGrades

---
+++ FinalGrades/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace FinalGrades
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            FileStream inFile = new FileStream(args[0], FileMode.Open);
+            StreamReader sReader = new StreamReader(inFile);
+            while (!sReader.EndOfStream)
+            {
+                string line = sReader.ReadLine().Trim();
+                string[] studentLine = Regex.Split(line, @"^(\w+\s*\w+)\s*,\s*(\w+\s*\w+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)$");
+                // wanted better regex that gets any number of grades
+                //  then
+                // copy grades to int array to pass to constructor
+                Student student = new Student(studentLine[1], studentLine[2], Convert.ToInt32(studentLine[3]), Convert.ToInt32(studentLine[4]), Convert.ToInt32(studentLine[5]), Convert.ToInt32(studentLine[6]), Convert.ToInt32(studentLine[7]));
+                students.Add(student);
+            }
+            // close inputs
+            sReader.Close(); inFile.Close();
+
+            // students implements IComparable sorting reverse order of FinalGrade
+            students.Sort();
+            foreach (Student student in students)
+            {
+                Console.Write("{0} {1} ({2}%) ({3}):", student.LastName.PadRight(10), student.FirstName.PadRight(10), student.FinalGrade, student.FinalLetter);
+                student.Grades.Sort(); student.Grades.Reverse();
+                foreach (int grade in student.Grades)
+                    Console.Write(" {0}", grade);
+                Console.WriteLine();
+            }
+
+            // wait for input to close console window
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FinalGrades/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalGrades
+{
+    public class Student : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int FinalGrade { get; set; }
+        public string FinalLetter { get; set; }
+        public List<int> Grades = new List<int>();
+
+        public Student(string firstName, string lastName, params int[] grades)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            foreach (int grade in grades)
+                Grades.Add(grade);
+            calculateFinalGrade();
+            calculateFinalLetter();
+        }
+
+        private void calculateFinalLetter()
+        {
+            if (FinalGrade > 93)
+                FinalLetter = "A";
+            else if (FinalGrade >= 90)
+                FinalLetter = "A-";
+            else if (FinalGrade >= 87)
+                FinalLetter = "B+";
+            else if (FinalGrade > 83)
+                FinalLetter = "B";
+            else if (FinalGrade >= 80)
+                FinalLetter = "B-";
+            else if (FinalGrade >= 77)
+                FinalLetter = "C+";
+            else if (FinalGrade > 73)
+                FinalLetter = "C";
+            else if (FinalGrade >= 70)
+                FinalLetter = "C-";
+            else if (FinalGrade >= 67)
+                FinalLetter = "D+";
+            else if (FinalGrade > 63)
+                FinalLetter = "D";
+            else if (FinalGrade >= 60)
+                FinalLetter = "D-";
+            else
+                FinalLetter = "F";
+        }
+
+        private void calculateFinalGrade()
+        {
+            double sum = 0.0;
+            foreach (int grade in Grades)
+                sum += grade;
+            FinalGrade = (int)Math.Round(sum / Grades.Count);
+        }
+
+        // sort highest grades first
+        public int CompareTo(object obj)
+        {
+            Student s = (Student)obj;
+            return s.FinalGrade - this.FinalGrade;
+        }
+    }
+}
